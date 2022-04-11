0c58764979c94daed316d18f34a02b8fa331f820
Savita Verma <82254618+vermasavita@users.noreply.github.com>
2022-04-11 10:08:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { Home } from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes>
       <Route path="/" element={<Home />}/>
         <Route path="products" element={<ProductListing />}/>
